75f2237d7b0b1f9c6c9b6177ac3406ed8aa0e8b2
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-06-04 22:16:40 -0400

    commit tree 0dc2b0f04d3ad209b5eb55999ce8ae57e37dfdda

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 function superbowlWin (objectArray) {
   // each object has a year and a result
   const wonGame = objectArray.find( function(g) { return g.result === 'W' });
-  if (wonGame) {
-    return wonGame.year;
-  } else {
-    return undefined;
-  };
+  return !!wonGame ? wonGame.year : undefined;
 }
