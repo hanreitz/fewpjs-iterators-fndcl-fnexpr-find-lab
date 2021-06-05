fef8f8ce9028b235554847086bd5d5eb6b0696c2
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-06-04 22:15:15 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin (objectArray) {
+  // each object has a year and a result
+  const wonGame = objectArray.find( function(g) { return g.result === 'W' });
+  if (wonGame) {
+    return wonGame.year;
+  } else {
+    return undefined;
+  };
+}
